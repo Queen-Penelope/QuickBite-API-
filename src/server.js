2a1015978bbd56102acfb15c34bec4703b9b08ea
Rym Bachrouch <icanseeyou9905@gmail.com>
2025-08-05 13:34:02 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { ENV } from "./config/env.js";            // if you have this
 import { router as favoritesRoute } from "./routes/favoritesRoute.js";
+import job from "./config/cron.js";
 
 const app = express();
 const port = ENV?.PORT ?? process.env.PORT ?? 5001;
 
+if(ENV.NODE_ENV === "production"){
+  job.start();
+}
+
 app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
+
 app.use("/api/favorites", favoritesRoute); 
 
 app.listen(port, () => {
